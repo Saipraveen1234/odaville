35843485b88984b1ed21b4819bd4f656c75eab04
Sai Praveen <yorssaipraveen@gmail.com>
2025-04-20 06:47:46 +0530

    SAve-Working

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get("/favicon.ico", (req, res) => {
   res.status(204).end(); // No content response
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`API available at http://localhost:${PORT}/api`);
